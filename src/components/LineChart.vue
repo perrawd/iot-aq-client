64a6d556979f02d9291126c4ed3f3cdc07d1425d
Per Rawdin <pr222iw@student.lnu.se>
2022-05-26 18:35:25 +0200

    Add data fetch method

--- src/components/LineChart.vue
+++ src/components/LineChart.vue
@@ -10,8 +10,18 @@
  * @author Per Rawdin <per.rawdin@student.lnu.se>
  * @version 1.0.0
  */
+import axios from 'axios'
 export default {
-  name: 'LineChart'
+  name: 'LineChart',
+  mounted () {
+    // Fetch data from API.
+    axios.get(process.env.VUE_APP_URL, {
+      auth: {
+        username: process.env.VUE_APP_USER,
+        password: process.env.VUE_APP_PASS
+      }
+    })
+  }
 }
 </script>
 
