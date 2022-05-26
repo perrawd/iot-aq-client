c9db50ea3e3e45cdea57bbacb51229b4ea6a79bd
Per Rawdin <pr222iw@student.lnu.se>
2022-05-26 18:38:58 +0200

    Add apexcharts module and config

--- src/components/LineChart.vue
+++ src/components/LineChart.vue
@@ -10,9 +10,14 @@
  * @author Per Rawdin <per.rawdin@student.lnu.se>
  * @version 1.0.0
  */
+import VueApexCharts from 'vue-apexcharts'
 import axios from 'axios'
+
 export default {
   name: 'LineChart',
+  components: {
+    apexcharts: VueApexCharts
+  },
   mounted () {
     // Fetch data from API.
     axios.get(process.env.VUE_APP_URL, {
@@ -21,6 +26,23 @@ export default {
         password: process.env.VUE_APP_PASS
       }
     })
+  },
+  data () {
+    return {
+      // Chart configuration.
+      chartOptions: {
+        stroke: {
+          curve: 'smooth'
+        },
+        chart: {
+          id: 'basic-bar'
+        },
+        xaxis: {
+          categories: null
+        }
+      },
+      series: null
+    }
   }
 }
 </script>
