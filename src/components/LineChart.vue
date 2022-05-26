d323f11c4e3ec6ed975e68532dfb1f94fd91f180
Per Rawdin <pr222iw@student.lnu.se>
2022-05-26 18:25:52 +0200

    Add LineChart component

---
+++ src/components/LineChart.vue
@@ -0,0 +1,20 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+/**
+ *
+ * LineChart component.
+ *
+ * @author Per Rawdin <per.rawdin@student.lnu.se>
+ * @version 1.0.0
+ */
+export default {
+  name: 'LineChart'
+}
+</script>
+
+<style>
+
+</style>
